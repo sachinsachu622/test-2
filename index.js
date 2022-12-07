de598362ae8b82d36552c88a98ae2a9f9c0443e8
sachin <sachinsachu622@gmail.com>
2022-12-07 11:01:59 +0530

    test-2

---
+++ index.js
@@ -0,0 +1,17 @@
+new gridjs.Grid({
+  columns: ["Name", "Email", "Phone Number"],
+  data: [
+    ["John", "john@example.com", "(353) 01 222 3333"],
+    ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
+    ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
+    ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
+    ["Afshin", "afshin@mail.com", "(353) 22 87 8356"],
+  ],
+  style:{
+    td:{
+        width:'700px',
+        height:'5px'
+    }
+  }
+}).render(document.getElementById("wrapper"));
+
